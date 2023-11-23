340a538f0405f4116500ea3a9b727c9cc5ffc6d3
astro175 <aspark400@gmail.com>
2023-11-23 12:40:46 +0100

    basic setup and header done

---
+++ landing-page/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header'
+
+export default function Header() {
+    return (
+        <header>
+            <div className='register-ctn'>
+                <h1>Kwara Digital Education</h1>
+            </div>
+            <div></div>
+        </header>
+    )
+}
---
+++ landing-page/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <p className='title'>KWARATECH ACADEMY</p>
+            <button className='register-btn'>Register</button>
+        </nav>
+    )
+}
